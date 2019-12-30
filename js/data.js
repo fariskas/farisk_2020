816242ebf1c33ba70bfcac00d45b9cafd6858c3a
fariskas <faris.kassim44@gmail.comgit config --global user.name Your>
2019-12-30 18:30:12 +0900

    first commit

---
+++ js/data.js
@@ -0,0 +1,160 @@
+var data_cat =	[
+					{
+						"title" : "Websites",
+						"code"	: "W", 
+					},	
+					{
+						"title" : "E-Commerce",
+						"code"	: "EC", 
+					},	
+					{
+						"title" : "Installations",
+						"code"	: "IN", 
+					},	
+					{
+						"title" : "Exhibitions",
+						"code"	: "EX", 
+					},	
+					{
+						"title" : "Portfolio",
+						"code"	: "PF", 
+					},	
+					{
+						"title" : "Motion Graphics",
+						"code"	: "MG", 
+					},	
+					{
+						"title" : "Design / Concept",
+						"code"	: "D", 
+					},	
+					{
+						"title" : "Development",
+						"code"	: "DV", 
+					},	
+			
+
+
+				]
+
+var data_work = [
+				{
+					"title" 		: "Amos Abel Yong",
+					"description"	: "Concept and development of an online portfolio with a content management system for Singapore-based photographer, Amos Abel Yong. ",
+					"url" 			: "http://ahmoo.se/",
+					"cat"			: ["w", "pf", "d", "dv"],
+					"path"			: "amos",
+					"media"			: ["0a.mp4", "1.jpg", "2.jpg"]
+				},	
+				{
+					"title" 		: "A Minor Thread",
+					"description"	: "A Minor Thread is a design practice based in Singapore that works on commercial and independent projects.<br><br>The site invites visitors to create their own ‘minor threads’ by drawing on the home page with a mouse pointer or their own touch devices.<br><br>Muted pixel pigment variations of each project is sampled and used in a subtle gradient overlay as a hover effect.",
+					"url" 			: "http://aminorthread.com/",
+					"cat"			: ["w", "pf", "d", "dv"],
+					"path"			: "amt",
+					"media"			: ["0a.mp4", "1.jpg", "2.jpg"]
+				},	
+				{
+					"title" 		: "OKOK Services",
+					"description"	: "A Minor Thread is a design practice based in Singapore that works on commercial and independent projects.<br><br>The site invites visitors to create their own ‘minor threads’ by drawing on the home page with a mouse pointer or their own touch devices.<br><br>Muted pixel pigment variations of each project is sampled and used in a subtle gradient overlay as a hover effect.",
+					"url" 			: "http://aminorthread.com/",
+					"cat"			: ["w", "pf", "d", "dv"],
+					"path"			: "amt",
+					"media"			: ["0a.mp4", "1.jpg", "2.jpg"]
+				},
+				{
+					"title" 		: null,
+				},			
+				{
+					"title" 		: null,
+				},	
+				{
+					"title" 		: "Mun Jiwoo",
+					"description"	: "A Minor Thread is a design practice based in Singapore that works on commercial and independent projects.<br><br>The site invites visitors to create their own ‘minor threads’ by drawing on the home page with a mouse pointer or their own touch devices.<br><br>Muted pixel pigment variations of each project is sampled and used in a subtle gradient overlay as a hover effect.",
+					"url" 			: "http://aminorthread.com/",
+					"cat"			: ["w", "pf", "d", "dv"],
+					"path"			: "amt",
+					"media"			: ["0a.mp4", "1.jpg", "2.jpg"]
+				},
+				{
+					"title" 		: null,
+				},	
+				{
+					"title" 		: "The Mindful Company",
+					"description"	: "A Minor Thread is a design practice based in Singapore that works on commercial and independent projects.<br><br>The site invites visitors to create their own ‘minor threads’ by drawing on the home page with a mouse pointer or their own touch devices.<br><br>Muted pixel pigment variations of each project is sampled and used in a subtle gradient overlay as a hover effect.",
+					"url" 			: "http://aminorthread.com/",
+					"cat"			: ["w", "ec", "d", "dv"],
+					"path"			: "amt",
+					"media"			: ["0a.mp4", "1.jpg", "2.jpg"]
+				},
+				{
+					"title" 		: "Demo Festival 2019",
+					"description"	: "A Minor Thread is a design practice based in Singapore that works on commercial and independent projects.<br><br>The site invites visitors to create their own ‘minor threads’ by drawing on the home page with a mouse pointer or their own touch devices.<br><br>Muted pixel pigment variations of each project is sampled and used in a subtle gradient overlay as a hover effect.",
+					"url" 			: "http://aminorthread.com/",
+					"cat"			: ["in", "ex", "mg", "d"],
+					"path"			: "amt",
+					"media"			: ["0a.mp4", "1.jpg", "2.jpg"]
+				},
+				{
+					"title" 		: null,
+				},	
+				{
+					"title" 		: "The Gentle Label",
+					"description"	: "A Minor Thread is a design practice based in Singapore that works on commercial and independent projects.<br><br>The site invites visitors to create their own ‘minor threads’ by drawing on the home page with a mouse pointer or their own touch devices.<br><br>Muted pixel pigment variations of each project is sampled and used in a subtle gradient overlay as a hover effect.",
+					"url" 			: "http://aminorthread.com/",
+					"cat"			: ["w", "ec", "d", "dv"],
+					"path"			: "amt",
+					"media"			: ["0a.mp4", "1.jpg", "2.jpg"]
+				},
+				{
+					"title" 		: "Red Bull Presents",
+					"description"	: "A Minor Thread is a design practice based in Singapore that works on commercial and independent projects.<br><br>The site invites visitors to create their own ‘minor threads’ by drawing on the home page with a mouse pointer or their own touch devices.<br><br>Muted pixel pigment variations of each project is sampled and used in a subtle gradient overlay as a hover effect.",
+					"url" 			: "http://aminorthread.com/",
+					"cat"			: ["mg", "d", "dv"],
+					"path"			: "amt",
+					"media"			: ["0a.mp4", "1.jpg", "2.jpg"]
+				},
+				{
+					"title" 		: null,
+				},
+				{
+					"title" 		: "Full of Luck Club",
+					"description"	: "A Minor Thread is a design practice based in Singapore that works on commercial and independent projects.<br><br>The site invites visitors to create their own ‘minor threads’ by drawing on the home page with a mouse pointer or their own touch devices.<br><br>Muted pixel pigment variations of each project is sampled and used in a subtle gradient overlay as a hover effect.",
+					"url" 			: "http://aminorthread.com/",
+					"cat"			: ["w", "d", "dv"],
+					"path"			: "amt",
+					"media"			: ["0a.mp4", "1.jpg", "2.jpg"]
+				},
+				{
+					"title" 		: "The Waumap Project",
+					"description"	: "A Minor Thread is a design practice based in Singapore that works on commercial and independent projects.<br><br>The site invites visitors to create their own ‘minor threads’ by drawing on the home page with a mouse pointer or their own touch devices.<br><br>Muted pixel pigment variations of each project is sampled and used in a subtle gradient overlay as a hover effect.",
+					"url" 			: "http://aminorthread.com/",
+					"cat"			: ["w", "in", "ex", "d", "dv"],
+					"path"			: "amt",
+					"media"			: ["0a.mp4", "1.jpg", "2.jpg"]
+				},
+				{
+					"title" 		: null,
+				},
+				{
+					"title" 		: "Summon - Temple of Echoes",
+					"description"	: "A Minor Thread is a design practice based in Singapore that works on commercial and independent projects.<br><br>The site invites visitors to create their own ‘minor threads’ by drawing on the home page with a mouse pointer or their own touch devices.<br><br>Muted pixel pigment variations of each project is sampled and used in a subtle gradient overlay as a hover effect.",
+					"url" 			: "http://aminorthread.com/",
+					"cat"			: ["w", "ex", "in", "d", "dv"],
+					"path"			: "amt",
+					"media"			: ["0a.mp4", "1.jpg", "2.jpg"]
+				},
+				{
+					"title" 		: null,
+				},
+				{
+					"title" 		: null,
+				},
+				{
+					"title" 		: "Bless You My Dear",
+					"description"	: "A Minor Thread is a design practice based in Singapore that works on commercial and independent projects.<br><br>The site invites visitors to create their own ‘minor threads’ by drawing on the home page with a mouse pointer or their own touch devices.<br><br>Muted pixel pigment variations of each project is sampled and used in a subtle gradient overlay as a hover effect.",
+					"url" 			: "http://aminorthread.com/",
+					"cat"			: ["w", "ec", "d", "dv"],
+					"path"			: "amt",
+					"media"			: ["0a.mp4", "1.jpg", "2.jpg"]
+				},
+			]
---
+++ js/scripts.js
@@ -0,0 +1,75 @@
+
+	var style = document.createElement('style');
+	style.type = 'text/css';
+
+	var style_to_add = "";
+		for (var ii = 0; ii < data_cat.length; ii++) {
+			style_to_add += '.has-cat_'+data_cat[ii].code.toLowerCase()+' .cat_'+data_cat[ii].code.toLowerCase()+' { visibility: visible; }';
+			style_to_add += '.filtered.filtered_'+data_cat[ii].code.toLowerCase()+' .cat_'+data_cat[ii].code.toLowerCase()+' { opacity:1; }';
+			style_to_add += '.filtered.filtered_'+data_cat[ii].code.toLowerCase()+' .filter_'+data_cat[ii].code.toLowerCase()+' { opacity:1; }';
+
+			$('.filter ul').append('\
+				<li class="filter_'+data_cat[ii].code.toLowerCase()+'" data-type="'+data_cat[ii].code+'"><span class="code">'+data_cat[ii].code+'</span><span class="cat">'+data_cat[ii].title+'</span></li>\
+			')
+		}
+		style.innerHTML = style_to_add;
+
+	document.getElementsByTagName('head')[0].appendChild(style);
+
+
+
+
+
+	for (var i = 0; i < data_work.length; i++) {
+		$('.section-right').append('\
+			<div class="grid grid-'+i+'">\
+			</div>\
+		')	
+
+		if (data_work[i].title) {
+			$('.grid-'+i).append('\
+				<span class="plus">\
+					<img src="assets/icn/plus.svg">\
+				</span>\
+				<span class="work-title">'+data_work[i].title+'</span>\
+				<ul class="work-graph">\
+				</ul>\
+			');
+
+			for (var z = 0; z < data_work[i].cat.length; z++) {
+				$('.grid-'+i+' .work-graph').addClass('has-cat_'+data_work[i].cat[z])
+			}
+
+			for (var y = 0; y < data_cat.length; y++) {
+				$('.grid-'+i+' .work-graph').append('\
+					<li class="cat_dot cat_'+data_cat[y].code.toLowerCase()+'">\
+						<div class="inner">\
+							<span class="dot"></span><span class="cat_title">'+data_cat[y].code+'</span>\
+						</div>\
+					</li>\
+				')
+			}
+		} else {
+			$('.grid-'+i).addClass('empty-grid')
+		}
+	}
+
+
+
+	$('.filter li').click(function() {
+		var to_filter = $(this).attr('data-type').toLowerCase();
+		
+		if ( !$('body').hasClass('filtered_'+to_filter) ) {
+			$('body').addClass('filtered filtered_'+to_filter)	
+		} else {
+			$('body').removeClass('filtered_'+to_filter)	
+		}
+
+		if ($('body').attr('class').indexOf("filtered_") == -1) {
+			$('body').removeClass('filtered')
+			$('.reset-filter, .filter-divider').removeClass('show')
+		} else {
+			$('.reset-filter, .filter-divider').addClass('show')
+		}
+		
+	})
