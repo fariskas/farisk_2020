83d3458653ba4c14ff87909ae6a4ae89985d6e39
Faris Kassim <faris@MacBook-Pro.local>
2020-12-21 00:22:04 +0900

    rtfm + trends update, remove gentle label and coma

--- js/data.js
+++ js/data.js
@@ -34,24 +34,29 @@ var data_cat = [
 ];
 
 var data_work = [
+
+
     {
-        title: 'Amos Abel Yong',
-        description: 'Concept and development of an online portfolio with a content management system for Singapore-based photographer, Amos Abel Yong.',
-        url: 'https://ahmoo.se/',
-        cat: ['w', 'pf', 'd', 'dv'],
-        path: 'amos',
+        title: 'Rememember To Forget',
+        description: '“Remember to Forget” is an ongoing personal project, archiving film images taken in the course of the past 10 years (2010-).<br><br>The work explores a certain present that is formed in its relation to the past, and if memories are intangible traces of that past, it becomes a bit more obvious why the malleability of memory has to be essentially disturbing.',
+        url: "https://remembertoforget.me",
+        cat: ['w', 'ex', 'd', 'dv'],
+        path: null,
         media: ['0.mp4'],
-        vimeo: '386013980',
+        vimeo: '493050692',
     },
+
+
     {
-        title: 'Mun Jiwoo',
-        description: 'Mun Jiwoo is a South Korean conceptual artist.<br><br>This website linearly journals her wanders through life with the mouse cursor acting as a color picker tool that changes the background color of the website depending on the part of the photograph the mouse hovers on.',
-        url: 'https://munjiwoo.com',
-        cat: ['w', 'pf', 'd', 'dv'],
-        path: 'jiwoo',
+        title: 'MONO X7',
+        description: 'Designed and developed a new playful, bi-lingual site for Mono X7 – a digital canvas for the digital age.',
+        url: 'https://mono.frm.fm/',
+        cat: ['w', 'ec', 'd', 'dv'],
+        path: 'tmc',
         media: ['0.mp4'],
-        vimeo: '430933119',
+        vimeo: '459880805',
     },
+
     {
         title: null,
     },
@@ -77,23 +82,28 @@ var data_work = [
         media: ['0.mp4'],
         vimeo: '430933957',
     },
+
+
+
     {
-        title: 'MONO X7',
-        description: 'Designed and developed a new playful, bi-lingual site for Mono X7 – a digital canvas for the digital age.',
-        url: 'https://mono.frm.fm/',
-        cat: ['w', 'ec', 'd', 'dv'],
-        path: 'tmc',
-        media: ['0.mp4'],
-        vimeo: '459880805',
-    },
-    {
-        title: 'A Minor Thread',
-        description: 'A Minor Thread is a design practice based in Singapore that works on commercial and independent projects.<br><br>The site invites visitors to create their own ‘minor threads’ by drawing on the home page with a mouse pointer or their own touch devices.<br><br>Muted pixel pigment variations of each project is sampled and used in a subtle gradient overlay as a hover effect.',
-        url: 'http://aminorthread.com/',
+        title: 'Mun Jiwoo',
+        description: 'Mun Jiwoo is a South Korean conceptual artist.<br><br>This website linearly journals her wanders through life with the mouse cursor acting as a color picker tool that changes the background color of the website depending on the part of the photograph the mouse hovers on.',
+        url: 'https://munjiwoo.com',
         cat: ['w', 'pf', 'd', 'dv'],
-        path: 'amt',
+        path: 'jiwoo',
         media: ['0.mp4'],
-        vimeo: '386014009',
+        vimeo: '430933119',
+    },
+
+
+    {
+        title: 'Amos Abel Yong',
+        description: 'Concept and development of an online portfolio with a content management system for Singapore-based photographer, Amos Abel Yong.',
+        url: 'https://ahmoo.se/',
+        cat: ['w', 'pf', 'd', 'dv'],
+        path: 'amos',
+        media: ['0.mp4'],
+        vimeo: '386013980',
     },
 
     {
@@ -105,6 +115,7 @@ var data_work = [
         media: ['0.mp4'],
         vimeo: '386014080',
     },
+
     {
         title: null,
     },
@@ -119,15 +130,19 @@ var data_work = [
         vimeo: '430935152',
     },
 
+
     {
-        title: 'Red Bull Music',
-        description: 'Red Bull Music Presents is a collection of shows that celebrates diverse music scenes in cities across the globe. I was comissioned to create some exploratory motion graphic visuals with code for the 2020 edition of the program.<br><br>Visuals done in code with THREE.js and cables.gl',
+        title: 'A Minor Thread',
+        description: 'A Minor Thread is a design practice based in Singapore that works on commercial and independent projects.<br><br>The site invites visitors to create their own ‘minor threads’ by drawing on the home page with a mouse pointer or their own touch devices.<br><br>Muted pixel pigment variations of each project is sampled and used in a subtle gradient overlay as a hover effect.',
         url: null,
-        cat: ['mg', 'd', 'dv'],
-        path: 'redbull',
+        cat: ['w', 'pf', 'd', 'dv'],
+        path: 'amt',
         media: ['0.mp4'],
-        vimeo: '386015613',
+        vimeo: '386014009',
     },
+
+
+
     {
         title: null,
     },
@@ -144,15 +159,30 @@ var data_work = [
     {
         title: null,
     },
+
+
+
     {
-        title: 'The Waumap Project',
-        description: 'The Waumap project archives a 10-year record of major commercial buildings near Wausan-ro, the location of Hongik University and presents this information as interactive ‘street-view’ images and motion typography.<br><br>WebGL visuals done with THREE.js<br><br>Project completed while working with Rebel9.',
-        url: 'https://waumap.netlify.com/',
-        cat: ['w', 'in', 'ex', 'd', 'dv'],
-        path: 'waumap',
+        title: 'Red Bull Music',
+        description: 'Red Bull Music Presents is a collection of shows that celebrates diverse music scenes in cities across the globe. I was comissioned to create some exploratory motion graphic visuals with code for the 2020 edition of the program.<br><br>Visuals done in code with THREE.js and cables.gl',
+        url: null,
+        cat: ['mg', 'd', 'dv'],
+        path: 'redbull',
         media: ['0.mp4'],
-        vimeo: '386014188',
+        vimeo: '386015613',
     },
+
+
+    // {
+    //     title: 'The Gentle Label',
+    //     description: 'The Gentle Label makes beautifully simple and natural skincare products for the whole family and home.<br><br>Development of the online shop was done on Shopify.',
+    //     url: null,
+    //     cat: ['w', 'ec', 'd', 'dv'],
+    //     path: 'tgl',
+    //     media: ['0.mp4'],
+    //     vimeo: '386014154',
+    // },
+
 
     {
         title: 'Full of Luck Club',
@@ -166,15 +196,29 @@ var data_work = [
     {
         title: null,
     },
+
+
     {
-        title: 'The Mindful Company',
-        description: "The Mindful Company is a website for individuals to join on a journey to live an inspired and mindful life. It also features an online store to buy thoughtfully designed reminder bands.<br><br>Nominated for <a href='https://www.awwwards.com/' target='_blank'>AWWWARDS</a> 2015 ecommerce website of the year",
-        url: 'https://www.mindful-company.com/',
-        cat: ['w', 'ec', 'd', 'dv'],
-        path: 'tmc',
+        title: 'Trends&Culture',
+        description: 'A playful holding site for Trends&Culture, a London and Paris based studio for strategy, technology and creativity.',
+        url: "https://trendsandculture.com/",
+        cat: ['w', 'd', 'dv'],
+        path: null,
         media: ['0.mp4'],
-        vimeo: '386012622',
+        vimeo: '493049797',
     },
+
+    // {
+    //     title: 'Coma Makeup',
+    //     description: 'Coma Makeup is a one-stop online beauty station for amazing quality cosmetics and beauty products at affordable prices.<br><br>Online store built on the Shopify ecommerce platform.',
+    //     url: null,
+    //     cat: ['w', 'ec', 'd', 'dv'],
+    //     path: 'coma',
+    //     media: ['0.mp4'],
+    //     vimeo: '386014044',
+    // },
+
+
     {
         title: 'MMCA Korea',
         description: 'MMCA - A website for the National Museum of Modern and Contemporary Art of Korea (MMCA) to showcase archive data submissions from an online survey done as part of the 50th anniversary celebrations of the art institution.<br><br>WebGL data visualization done with THREE.js and oimi.js<br><br>Project completed while working with Rebel9.',
@@ -184,6 +228,38 @@ var data_work = [
         media: ['0.mp4'],
         vimeo: '386014127',
     },
+
+    {
+        title: 'The Waumap Project',
+        description: 'The Waumap project archives a 10-year record of major commercial buildings near Wausan-ro, the location of Hongik University and presents this information as interactive ‘street-view’ images and motion typography.<br><br>WebGL visuals done with THREE.js<br><br>Project completed while working with Rebel9.',
+        url: 'https://waumap.netlify.com/',
+        cat: ['w', 'in', 'ex', 'd', 'dv'],
+        path: 'waumap',
+        media: ['0.mp4'],
+        vimeo: '386014188',
+    },
+
+    {
+        title: null,
+    },
+
+    {
+        title: 'The Mindful Company',
+        description: "The Mindful Company is a website for individuals to join on a journey to live an inspired and mindful life. It also features an online store to buy thoughtfully designed reminder bands.<br><br>Nominated for <a href='https://www.awwwards.com/' target='_blank'>AWWWARDS</a> 2015 ecommerce website of the year",
+        url: 'https://www.mindful-company.com/',
+        cat: ['w', 'ec', 'd', 'dv'],
+        path: 'tmc',
+        media: ['0.mp4'],
+        vimeo: '386012622',
+    },
+
+    {
+        title: null,
+    },
+    {
+        title: null,
+    },
+
     {
         title: 'Bless You My Dear',
         description: 'Bless You My Dear – A curation of sterling silver jewelry and an expression of wearable frankness.<br><br>Online store built on the Shopify ecommerce platform.',
@@ -194,33 +270,6 @@ var data_work = [
         vimeo: '386015212',
     },
 
-    {
-        title: null,
-    },
-    {
-        title: 'Coma Makeup',
-        description: 'Coma Makeup is a one-stop online beauty station for amazing quality cosmetics and beauty products at affordable prices.<br><br>Online store built on the Shopify ecommerce platform.',
-        url: null,
-        cat: ['w', 'ec', 'd', 'dv'],
-        path: 'coma',
-        media: ['0.mp4'],
-        vimeo: '386014044',
-    },
-    {
-        title: null,
-    },
-    {
-        title: null,
-    },
-    {
-        title: 'The Gentle Label',
-        description: 'The Gentle Label makes beautifully simple and natural skincare products for the whole family and home.<br><br>Development of the online shop was done on Shopify.',
-        url: null,
-        cat: ['w', 'ec', 'd', 'dv'],
-        path: 'tgl',
-        media: ['0.mp4'],
-        vimeo: '386014154',
-    },
 
     {
         title: null,
